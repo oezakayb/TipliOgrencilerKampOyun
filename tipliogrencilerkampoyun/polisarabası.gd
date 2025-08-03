7b45b5a4515b7f5e5d5ec4c07841ed94b60adc0f
Özgür Ege Polat <ozguregepolat@gmail.com>
2025-08-03 16:50:32 +0300

    polis

---
+++ tipliogrencilerkampoyun/polisarabası.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@export var hareket_hizi : float = 20.0
+@export var tekrarlar : bool = true
+@export var sprite_node_path : NodePath
+
+var sprite : Sprite2D
+
+func _ready():
+	sprite = get_node(sprite_node_path)
+
+func _physics_process(delta: float) -> void:
+	if sprite and global_position.x <= -sprite.texture.get_width():
+		if tekrarlar:
+			global_position.x = sprite.texture.get_width() - 1
+		else:
+			queue_free()
+	global_position.x -= hareket_hizi
